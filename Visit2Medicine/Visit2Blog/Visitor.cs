8f965bbeb7dcf17ee52238fe122d39688999021f
wuyou201400 <504032263@qq.com>
2016-06-07 11:53:37 +0800

    FirstPost
    将源代码首次提交

---
+++ Visit2Medicine/Visit2Blog/Visitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visit2Blog
+{
+    //抽象访问者(Visitor)
+    abstract class Visitor
+    {
+        public abstract void VisitBlog(AbstarctElementBlog element);
+    }
+    //具体访问者(ConcreteVisitor)
+    class WebVisitor : Visitor
+    {
+        public override void VisitBlog(AbstarctElementBlog element)
+        {
+            Console.WriteLine("通过电脑web网站方式访问Blog:" + element.BlogName);
+        }
+    }
+    //具体访问者(ConcreteVisitor)
+    class WapVisitor : Visitor
+    {
+        public override void VisitBlog(AbstarctElementBlog element)
+        {
+            Console.WriteLine("通过手机wap网站方式访问Blog:" + element.BlogName);
+
+        }
+    }
+}
